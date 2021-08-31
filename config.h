3bc10af45b7382b298c6604d9fd3540249657867
sadikeey <sadiksaifi205@gmail.com>
2021-08-31 07:25:59 +0530

    misc

--- config.h
+++ config.h
@@ -4,9 +4,9 @@
 /* appearance */
 static const unsigned int borderpx    = 2;      /* border pixel of windows */
 static const unsigned int snap        = 32;     /* snap pixel */
-static const unsigned int gappih      = 10;     /* horiz inner gap between windows */
-static const unsigned int gappiv      = 10;     /* vert inner gap between windows */
-static const unsigned int gappoh      = 6;      /* horiz outer gap between windows and screen edge */
+static const unsigned int gappih      = 7;      /* horiz inner gap between windows */
+static const unsigned int gappiv      = 7;      /* vert inner gap between windows */
+static const unsigned int gappoh      = 7;      /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov      = 15;     /* vert outer gap between windows and screen edge */
 static const int smartgaps            = 0;      /* 1 means no outer gap when there is only one window */
 static const int showbar              = 1;      /* 0 means no bar */
@@ -34,7 +34,7 @@ static const char grey[]              = "#222222";
 static const char *colors[][3]        = {
 	/*                    fg     bg     border   */
 	[SchemeNorm]     = { cream, black, black }, //inactive
-	[SchemeSel]      = { lcream,black, cream }, //active
+	[SchemeSel]      = { lcream,black, lcream },//active
 };
 static const unsigned int alphas[][3] = {
 	/*                    fg      bg        border     */
