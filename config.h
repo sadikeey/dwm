785d2270f04663873beb47e84cf4dd2576610ed9
sadikeey <sadiksaifi205@gmail.com>
2021-08-31 17:06:59 +0530

    misc

--- config.h
+++ config.h
@@ -4,9 +4,9 @@
 /* appearance */
 static const unsigned int borderpx    = 2;      /* border pixel of windows */
 static const unsigned int snap        = 32;     /* snap pixel */
-static const unsigned int gappih      = 7;      /* horiz inner gap between windows */
-static const unsigned int gappiv      = 7;      /* vert inner gap between windows */
-static const unsigned int gappoh      = 7;      /* horiz outer gap between windows and screen edge */
+static const unsigned int gappih      = 6;      /* horiz inner gap between windows */
+static const unsigned int gappiv      = 6;      /* vert inner gap between windows */
+static const unsigned int gappoh      = 6;      /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov      = 15;     /* vert outer gap between windows and screen edge */
 static const int smartgaps            = 0;      /* 1 means no outer gap when there is only one window */
 static const int showbar              = 1;      /* 0 means no bar */
@@ -66,9 +66,6 @@ static const Layout layouts[]   = {
 	{ "  [M]       =",      monocle },
 };
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
 /* Defining Application */
 static char dmenumon[2]       = "0";    /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
@@ -89,6 +86,9 @@ static const char *downbrightness[] = { "/usr/bin/xbacklight",  "-dec",         
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* Key Bindings */
 static Key keys[] = {
